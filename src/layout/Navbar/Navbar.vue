ba6b663b7618ac79ad7741cc1c659b1f1f8ecaaa
marsou001 <marsou431@gmail.com>
2022-05-16 17:22:55 +0100

    wip: navbar

---
+++ src/layout/Navbar/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <header>
+    <div class="search text-right">
+      <input type="search" name="search" id="search" class="border-2 border-blue-400" />
+    </div>
+    <div class="header-nav flex justify-between">
+      <div class="logo">
+        <!-- <font-awesome-icon icon="fa-solid fa-bars" /> -->
+        logo
+      </div>
+      <div class="hamburger-menu md:hidden" @click="toggleShowDropdownMenu">
+        hamburger
+      </div>
+      <nav class="hidden md:block">
+        <ul class="text-blue-400">
+          <li class="inline mx-3">HOME</li>
+          <li class="inline mx-3">CUPS</li>
+          <li class="inline mx-3">SPOONS</li>
+          <li class="inline mx-3">LIDS</li>
+          <li class="inline mx-3">ACCESSORIES</li>
+          <li class="inline mx-3">MARKETING</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <nav v-if="showDropdownMenu" class="md:hidden">
+    <div class="search">
+      <input type="search" name="search" id="search" />
+    </div>
+    <ul class="text-blue-400">
+      <li>HOME</li>
+      <li>CUPS</li>
+      <li>SPOONS</li>
+      <li>LIDS</li>
+      <li>ACCESSORIES</li>
+      <li>MARKETING</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data() {
+    return {
+      showDropdownMenu: true,
+    }
+  },
+  methods: {
+    toggleShowDropdownMenu() {
+      this.showDropdownMenu = !this.showDropdownMenu;
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/tailwind.css';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+// createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
 createApp(App).mount('#app')
