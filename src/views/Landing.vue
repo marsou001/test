1f98a322bdcdf90d4098e282c340111ca4d3ca53
marsou001 <marsou431@gmail.com>
2022-05-17 08:22:00 +0100

    wip: hero carousel

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,12 +1,13 @@
 <template>
-  <section class="bg-cyan-100">
-    <div class="carousel flex flex-col md:flex-row">
-      <div class="picture w-1/2 bg-green-300 p-3"></div>
-      <div class="description p-3">
-        <h1>Suspendisse interdum rutrum nise.</h1>
-        <p>Ut tellus quam, venenatis non tempus vitae, volupat blandit libero eget elos</p>
+  <section>
+    <div class="carousel bg-weak flex flex-col md:flex-row md:justify-between">
+      <div class="picture md:w-1/2 bg-green-300 p-3"></div>
+      <div class="description md:text-right p-3">
+        <h1 class="text-normal text-2xl font-bold">Suspendisse interdum rutrum nise.</h1>
+        <p class="text-normal">Ut tellus quam, venenatis non tempus vitae, volupat blandit libero eget elos</p>
       </div>
     </div>
+    <div class="slide">hhh</div>
   </section>
 </template>
 
