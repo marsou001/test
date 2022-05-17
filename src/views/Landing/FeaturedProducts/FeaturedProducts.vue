b9fad95914988bf5d73756667f4a024593343932
marsou001 <marsou431@gmail.com>
2022-05-17 15:07:52 +0100

    style: add arrow to view all

--- src/views/Landing/FeaturedProducts/FeaturedProducts.vue
+++ src/views/Landing/FeaturedProducts/FeaturedProducts.vue
@@ -2,12 +2,16 @@
   <section class="featured-products mb-6">
     <h1 class="text-normal text-3xl font-bold pb-9">Featured Products</h1>
 
-    <div class="featured-products-grid-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+    <div class="featured-products-grid-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mb-6">
       <FeaturedProduct v-for="product in featuredProducts" :key="product.name" :product="product" />
     </div>
 
     <div class="view-all">
-      <router-link :to="featuredProductsRoute" class="text-normal text-xl font-bold">View All</router-link>
+      <router-link :to="featuredProductsRoute" class="text-normal text-xl font-bold">
+        <font-awesome-icon :icon="['fas', 'arrow-right']" class="mr-2" />
+        <br />
+        <span>View All</span>
+      </router-link>
     </div>
   </section>
 </template>
