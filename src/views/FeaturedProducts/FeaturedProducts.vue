448d95e631bef121c3f28c813ea099938e460d82
marsou001 <marsou431@gmail.com>
2022-05-17 10:56:56 +0100

    feat: featured products with filters

--- src/views/FeaturedProducts/FeaturedProducts.vue
+++ src/views/FeaturedProducts/FeaturedProducts.vue
@@ -1,30 +1,32 @@
 <template>
   <Category />
   
-  <section class="flex flex-col md:flex-row md:justify-between">
-    <aside>
-      <h3 class="text-normal bg-weak py-1 px-2">FILTER NAME</h3>
+  <section class="flex flex-col lg:flex-row md:justify-between mb-9">
+    <aside class="lg:w-60 lg:mr-3 mb-3">
+      <h3 class="text-normal bg-weak text-left py-1 px-2">FILTER NAME</h3>
       <ul>
-        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
-        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
-        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
-        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
+        <li class="text-normal text-left sm:inline-block lg:my-1 px-2 lg:block">Option One</li>
+        <li class="text-normal text-left sm:inline-block lg:my-1 px-2 lg:block">Option One</li>
+        <li class="text-normal text-left sm:inline-block lg:my-1 px-2 lg:block">Option One</li>
+        <li class="text-normal text-left sm:inline-block lg:my-1 px-2 lg:block">Option One</li>
       </ul>
     </aside>
 
-    <div class="featured-products">
-      hhhh
+    <div class="featured-products grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ml-3">
+      <FeaturedProduct v-for="i in 6" :key="i" />
     </div>
   </section>
 </template>
 
 <script>
 import Category from './Category/Category';
+import FeaturedProduct from '@/components/FeaturedProduct.vue';
 
 export default {
   name: 'featured-products',
   components: {
     Category,
+    FeaturedProduct,
   },
 }
 </script>
