a321e3ba6479c166cb5dd4020983b5af7cc73e03
marsou001 <marsou431@gmail.com>
2022-05-17 10:34:03 +0100

    feat: filters

--- src/views/FeaturedProducts/FeaturedProducts.vue
+++ src/views/FeaturedProducts/FeaturedProducts.vue
@@ -1,5 +1,21 @@
 <template>
   <Category />
+  
+  <section class="flex flex-col md:flex-row md:justify-between">
+    <aside>
+      <h3 class="text-normal bg-weak py-1 px-2">FILTER NAME</h3>
+      <ul>
+        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
+        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
+        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
+        <li class="text-normal text-left sm:inline-block my-1 sm:mx-2 md:block">Option One</li>
+      </ul>
+    </aside>
+
+    <div class="featured-products">
+      hhhh
+    </div>
+  </section>
 </template>
 
 <script>
