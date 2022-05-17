bd16a373685c683385cc65a6a1d1056e12d5faef
marsou001 <marsou431@gmail.com>
2022-05-17 12:17:08 +0100

    feat: product head

--- src/views/FeaturedProduct/FeaturedProduct.vue
+++ src/views/FeaturedProduct/FeaturedProduct.vue
@@ -1,5 +1,29 @@
 <template>
-  gggg
+  <div class="nav-tree text-normal text-xs text-left mb-1">
+    Home > Category Name > Product Name
+  </div>
+
+  <div class="product">
+    <div class="pictures">
+      <div class="main-picture bg-weak w-full h-56 mb-6"></div>
+      <div class="other-pictures grid grid-cols-4 gap-6">
+        <div class="picture bg-weak h-16 sm:h-32"></div>
+        <div class="picture bg-weak h-16 sm:h-32"></div>
+        <div class="picture bg-weak h-16 sm:h-32"></div>
+        <div class="picture bg-weak h-16 sm:h-32"></div>
+      </div>
+    </div>
+
+    <div class="information">
+      <div class="head">
+        <h1 class="text-normal text-5xl font-bold">Product title</h1>
+        <h3 class="text-normal">Lorem ipsum dolor sit subtitle</h3>
+      </div>
+    </div>
+  </div>
+  
+  <hr class="mt-1 mb-6" />
+  
 </template>
 
 <script>
