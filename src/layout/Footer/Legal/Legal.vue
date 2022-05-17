0b77808f275cf8356bab58f1f703aa747e885056
marsou001 <marsou431@gmail.com>
2022-05-17 11:24:47 +0100

    style: footer adjustments

--- src/layout/Footer/Legal/Legal.vue
+++ src/layout/Footer/Legal/Legal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="legal">
+  <div class="legal text-normal text-xs">
     <div class="links"><a href="#">Privacy policy</a> &nbsp; | &nbsp; <a href="#">Terms & conditions</a></div>
     <div class="copyright">Cpyright 2013 Lorem Ipsum dolor sit amet</div>
   </div> 
